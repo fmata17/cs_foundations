ccbea16636a59a1278888fe2f87248fed2131e76
Fredy Mata <fredymata17@icloud.com>
2025-10-16 00:07:40 -0500

    Started working on 42

---
+++ 42_trapping_rain_water/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+// You are given an array of non-negative integers height which represent an elevation map.
+// Each value height[i] represents the height of a bar, which has a width of 1.
+
+// Return the maximum area of water that can be trapped between the bars.
+class Solution
+// height = [0,2,0,3,1,0,1,3,2,1]
+// output = 9
+{
+public:
+    int trap(vector<int> &height)
+    {
+    }
+};
