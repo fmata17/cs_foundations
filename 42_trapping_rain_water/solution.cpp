ff9e4a702e6b972a958e44185ea7fd2dabcdb35d
Fredy Mata <fredymata17@icloud.com>
2025-10-21 23:03:15 -0500

    Progress in problem 42

--- 42_trapping_rain_water/solution.cpp
+++ 42_trapping_rain_water/solution.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 // You are given an array of non-negative integers height which represent an elevation map.
@@ -12,5 +13,31 @@ class Solution
 public:
     int trap(vector<int> &height)
     {
+      int area = 0;
+      vector<int> max_left(height.size(), 0);
+      int cur_max = height[0];
+      for (int i = 1; i < height.size(); ++i)
+      {
+        max_left[i] = cur_max;
+        cur_max = max(cur_max, height[i]);
+      }
+      
+      // TODO find max_right and then min of both
+
+      for(int num : max_left)
+      {
+        cout << num << " ";
+      }
+      cout << endl;
+
+      return -1;
     }
 };
+
+
+int main () {
+  Solution s;
+  vector<int> nums{0,2,0,3,1,0,1,3,2,1};
+  s.trap(nums);
+  return 0;
+}
