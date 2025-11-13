2c7a6b18d318060b6a552c1b4b5bc817f4fe15c7
Fredy Mata <fredymata17@icloud.com>
2025-11-13 02:36:47 -0600

    Reviewed and added instructions to problem 238

--- algorithms/coding_challenges/238_product_of_array_except_self/solution.cpp
+++ algorithms/coding_challenges/238_product_of_array_except_self/solution.cpp
@@ -1,3 +1,16 @@
+// Given an integer array nums, return an array answer such that answer[i]
+// is equal to theproduct of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Follow up: Can you solve the problem in O(1) extra space complexity?
+// (The output array does not count as extra space for space complexity analysis.)
+
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -32,7 +45,7 @@ int main(int argc, char const *argv[])
     Solution s;
     vector<int> vec = {1, 3, 5, 10};
     vec = s.productExceptSelf(vec);
-    for (int n: vec)
+    for (int n : vec)
     {
         cout << n << " ";
     }
